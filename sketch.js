290a54175f309e00b7c16c69627459f0021512fd
nilodude <alphone@protonmail.com>
2020-10-23 23:25:33 +0200

    rave
    added random() to RGB value

--- sketch.js
+++ sketch.js
@@ -31,39 +31,40 @@ function setup() {
   // slider.style('width', '80px');
   createCanvas(width, height, WEBGL);
   fov = PI / 6;
-  escala = 5.2;
+  escala = 4;
+  camZ = 13 / 4 * (height / 2) / tan(fov / 2);
+  perspective(fov, width / height, camZ / 2, camZ * 10);
+  camera(camX, camY, camZ, posX, posY, posZ, 0, 1, 0);
+
 }
 
 
 function draw() {
   //camZ = slider.value() / 4 * (height / 2) / tan(fov / 2);
-  camZ = 13 / 4 * (height / 2) / tan(fov / 2);
-  perspective(fov, width / height, camZ / 2, camZ * 10);
-  camera(camX, camY, camZ, posX, posY, posZ, 0, 1, 0);
-  r = map(sin(radians(frameCount)), 0, 1, 0, 180);
-  g = map(cos(radians(frameCount) + PI / 3), 0, 1, 0, 40);
-  b = map(sin(radians(frameCount) + PI / 7), 0, 1, 0, 50);
-  clear();
-  //background(r, g, b, 70);
-  // orbitControl();
 
-  ambientLight(255);
+
+  r = map(cos(radians(frameCount / random(1, 8))), 0, 1, 100, 180);
+  g = map(cos(radians(frameCount / random(1, 7)) + PI / 7.1), 0, 1, 50, 140);
+  b = map(cos(radians(frameCount / random(1, 9)) + PI / 7), 0, 1, 50, 150);
+  clear();
+  background(r, g, b, 70);
+  // orbitControl();
+  ambientLight(r, g, b);
   pointLight(10, 20, 20, -10000, 10000, 0);
   noStroke();
 
 
   push(); //para aplicar la rotación a los dos elementos
-  // rotateY(radians(frameCount) / 9);
-
-  // rotateX(0.15 * cos(radians(frameCount) / 2));
+  rotateY(radians(frameCount) / 9);
+  rotateX(0.15 * cos(radians(frameCount) / 2));
 
   //portada
-  // push(); {
-  //   translate(0, offsetY, 1); // parriba (y) 
-  //   texture(cover);
-  //   box(tam, tam, 9, 1000, 1000);
-  // }
-  // pop();
+  push(); {
+    translate(0, offsetY, 2.4); // parriba (y) 
+    texture(cover);
+    box(tam, tam, 9, 1000, 1000);
+  }
+  pop();
 
   // dentroFuera = keyIsPressed && keyCode === SHIFT ? 0.7 * (mouseY - height) : dentroFuera;
   limitao = Math.min(Math.max(-0.95 * (mouseY - height), 0), height);
@@ -71,7 +72,7 @@ function draw() {
   //disco
   push(); {
     push(); { //vinilo
-      translate(dentroFuera, offsetY, -1.125);
+      translate(0, offsetY - dentroFuera, -1.125);
       fill(0);
       // rotateX(PI / 2);
       // cylinder(tam / 2, 4, 1000, 1000);
@@ -82,35 +83,37 @@ function draw() {
     pop();
     galletaSize = tam / 2.5;
     push(); { //galleta A
-      translate(dentroFuera, offsetY, 1.126);
+      translate(0, offsetY - dentroFuera, 2.9);
       fill(250, 250, 255);
       ellipse(0, 0, galletaSize);
-      fill(r, g, b, 70);
-      translate(0, 0, 0.007);
-      ellipse(0, 0, galletaSize / 10);
+      // fill(r, g, b, 70);
+      // translate(0, 0, 0.007);
+      // ellipse(0, 0, galletaSize / 10);
     }
     pop();
 
     push(); { //galleta B
-      translate(dentroFuera, offsetY, -5);
-      fill(250, 250, 255);
+      translate(0, offsetY - dentroFuera, -5);
+      fill(50, 50, 55);
       ellipse(0, 0, galletaSize);
-      fill(r, g, b, 70);
-      translate(0, 0, -0.1);
-      ellipse(0, 0, galletaSize / 10);
+      // fill(r, g, b, 70);
+      // translate(0, 0, -0.1);
+      // translate(0, 0, -0.1);
+      // ellipse(0, 0, galletaSize / 10);
     }
     pop();
   }
   pop();
   //contraportada
-  push();
-  translate(0, offsetY, -9); //idem + padentro (z)
-  // rotateY(PI);
-  rotateY(PI / 2);
-  texture(back);
-  // box(tam, tam, 9, 1000, 1000);
-  scale(escala);
-  model(coverM);
+  push(); {
+    translate(0, offsetY, -5); //idem + padentro (z)
+    rotateY(PI);
+    // rotateY(PI / 2);
+    texture(back);
+    box(tam, tam, 9, 1000, 1000);
+    // scale(escala);
+    // model(coverM);
+  }
   pop();
   pop();
 
